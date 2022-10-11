a8d67754ff834f564ed1b1277f5b921431ae7ef3
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2022-10-11 16:31:46 +0000

    add readme.

---
+++ java/examples.java
@@ -0,0 +1,13 @@
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+    /*
+    This file contains examples of how to interact with Lexigram APIs in Java.
+
+    Each of the examples inspect the responses printing some relevant properties.
+    For comprehensive documentation that includes information of all API responses
