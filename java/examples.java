ac2b6e17ea3aa738fbfea2df40b4c4f56e7e542a
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2022-10-12 19:34:10 +0000

    Prepare 0.3.1 release

--- java/examples.java
+++ java/examples.java
@@ -11,3 +11,7 @@ import org.json.JSONObject;
 
     Each of the examples inspect the responses printing some relevant properties.
     For comprehensive documentation that includes information of all API responses
+    please visit http://docs.lexigram.io
+    */
+public class examples {
+    /*
