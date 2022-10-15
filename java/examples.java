3c472e193bcbafd4b6dee51141f91fd2bc77f5fd
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2022-10-15 05:25:27 +0000

    ALSA: timer: Fix link corruption due to double start or stop

--- java/examples.java
+++ java/examples.java
@@ -15,3 +15,9 @@ import org.json.JSONObject;
     */
 public class examples {
     /*
+    Replace YOUR_KEY_HERE with your API KEY from your user profile at https://app.lexigram.io
+    */
+    private static String API_KEY = "Bearer " + "YOUR_KEY_HERE";
+
+    public static void main(String[] args) throws UnirestException {
+        exampleEntityExtraction();
