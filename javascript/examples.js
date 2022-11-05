745f8255d8d3cfe3c8358c92b63c2471336e3357
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2022-11-05 21:54:46 +0000

    Incorporate review comments

---
+++ javascript/examples.js
@@ -0,0 +1,9 @@
+
+/*
+This file contains examples of how to interact with Lexigram APIs in Javacript.
+
+Each of the examples inspect the responses printing some relevant properties.
+For comprehensive documentation that includes information of all API responses
+please visit http://docs.lexigram.io
+*/
+
