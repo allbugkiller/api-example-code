05bfaeec36acb0babc67874af32ce29e9ad8e4ea
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2022-11-12 07:37:29 +0000

    fixed version

--- javascript/examples.js
+++ javascript/examples.js
@@ -24,3 +24,10 @@ function exampleEntityExtraction(){
 
   var httpRequest = new XMLHttpRequest();
   httpRequest.open("POST", url, true);
+  httpRequest.setRequestHeader("Content-Type", "application/json");
+  httpRequest.setRequestHeader("Authorization", apiKey);
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState != 4 || httpRequest.status != 200) {
+      return;
+    }
+    var response = JSON.parse(httpRequest.responseText);
