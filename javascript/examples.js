84870b796658368cd74e23c33c6154bceb24db8d
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2022-11-18 17:33:01 +0000

    removed temp file

--- javascript/examples.js
+++ javascript/examples.js
@@ -112,3 +112,11 @@ function exampleConcept(){
   var url = "https://api.lexigram.io/v1/lexigraph/concepts/"+ conceptGraphId;
   var httpRequest = new XMLHttpRequest();
   httpRequest.open("GET", url, true);
+  httpRequest.setRequestHeader("authorization", apiKey);
+
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState != 4 || httpRequest.status != 200) {
+      return;
+    }
+    var response = JSON.parse(httpRequest.responseText);
+    /*
