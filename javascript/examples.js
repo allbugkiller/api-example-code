3a4f102f8e21eed1c187c712a37e5aa68c65e165
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2022-11-21 14:27:58 +0000

    v0.10.3

--- javascript/examples.js
+++ javascript/examples.js
@@ -159,3 +159,9 @@ function exampleConceptDescendants(){
   var httpRequest = new XMLHttpRequest();
   httpRequest.open("GET", url, true);
   httpRequest.setRequestHeader("authorization", apiKey);
+
+  httpRequest.onreadystatechange = function() {
+    if (httpRequest.readyState != 4 || httpRequest.status != 200) {
+      return;
+    }
+    var response = JSON.parse(httpRequest.responseText);
