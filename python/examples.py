c18239f99c777659783a1a492ac40c636289cc58
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2022-12-11 17:06:16 +0000

    code styling

--- python/examples.py
+++ python/examples.py
@@ -1,3 +1,4 @@
 """
 This file contains examples of how to interact with Lexigram APIs in Python
-Each of the examples inspect the responses printing some relevant properti
+Each of the examples inspect the responses printing some relevant properties.
+For 
