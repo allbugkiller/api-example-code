4d23e87f546500a96b96706f6810dad3a30f6514
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2022-12-13 21:36:43 +0000

    SYSTEM: Jenkins configuration files updated

--- python/examples.py
+++ python/examples.py
@@ -1,4 +1,4 @@
 """
 This file contains examples of how to interact with Lexigram APIs in Python
 Each of the examples inspect the responses printing some relevant properties.
-For comprehensive documentation tha
+For comprehensive documentation that incl
