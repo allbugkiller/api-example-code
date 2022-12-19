74de8e5cf6621a1793f472e9d5f900cb94d7b557
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2022-12-19 17:34:24 +0000

    Package updates

--- python/examples.py
+++ python/examples.py
@@ -1,4 +1,4 @@
 """
 This file contains examples of how to interact with Lexigram APIs in Python
 Each of the examples inspect the responses printing some relevant properties.
-For comprehensive documentation that includes information of all API re
+For comprehensive documentation that includes information of all API response
