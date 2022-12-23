fc2520cbd72da4b7b1467e7847f8ac7a94158b99
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2022-12-23 19:36:15 +0000

    Add footer.

--- python/examples.py
+++ python/examples.py
@@ -2,4 +2,4 @@
 This file contains examples of how to interact with Lexigram APIs in Python
 Each of the examples inspect the responses printing some relevant properties.
 For comprehensive documentation that includes information of all API responses
-please v
+please visit ht
