167a3737a9d81b97eeabca800a181e243dcafaff
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2022-12-25 20:09:09 +0000

    Funceble Test - Autosave

--- python/examples.py
+++ python/examples.py
@@ -2,4 +2,4 @@
 This file contains examples of how to interact with Lexigram APIs in Python
 Each of the examples inspect the responses printing some relevant properties.
 For comprehensive documentation that includes information of all API responses
-please visit http://docs
+please visit http://docs.lexigram.
