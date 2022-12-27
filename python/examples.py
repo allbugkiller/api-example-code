7c8fd0fa86bb6d00fc00603f5b006e657ce19c42
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2022-12-27 21:53:25 +0000

    removed unused variable

--- python/examples.py
+++ python/examples.py
@@ -3,4 +3,6 @@ This file contains examples of how to interact with Lexigram APIs in Python
 Each of the examples inspect the responses printing some relevant properties.
 For comprehensive documentation that includes information of all API responses
 please visit http://docs.lexigram.io
-""
+"""
+
+import
