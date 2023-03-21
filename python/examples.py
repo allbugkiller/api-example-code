ead42f56f89492c5d3bb1844e7f4e5af09dd0b2b
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2023-03-21 08:55:48 +0000

    fix(package): update chalk to version 2.0.0

--- python/examples.py
+++ python/examples.py
@@ -21,3 +21,4 @@ def exampleEntityExtraction():
 
     r = requests.post(url, data=json.dumps({'text': text}),
                       headers={'Authorization': apiKey, 'Content-Type': 'application/json'})
+    res
