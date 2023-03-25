d78f54e35287e9ea3bad01efcf0ae2c497dc4e2e
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2023-03-25 20:27:55 +0000

    prueba2

--- python/examples.py
+++ python/examples.py
@@ -23,4 +23,4 @@ def exampleEntityExtraction():
                       headers={'Authorization': apiKey, 'Content-Type': 'application/json'})
     response = json.loads(r.text)
 
-    '''For loop that inspects
+    '''For loop that inspects the resp
