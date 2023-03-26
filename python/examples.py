88329e807dfc06507aa5f794ad42b172ad0856ab
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2023-03-26 20:00:20 +0000

    updated deps

--- python/examples.py
+++ python/examples.py
@@ -23,4 +23,4 @@ def exampleEntityExtraction():
                       headers={'Authorization': apiKey, 'Content-Type': 'application/json'})
     response = json.loads(r.text)
 
-    '''For loop that inspects the response printing to console the 
+    '''For loop that inspects the response printing to console the extracted
