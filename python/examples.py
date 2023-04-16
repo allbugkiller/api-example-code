bf2c22258d413caf7793d6bf3a2287c15cb60f1d
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2023-04-16 08:45:16 +0000

    Update to version 1.0.1

--- python/examples.py
+++ python/examples.py
@@ -28,4 +28,4 @@ def exampleEntityExtraction():
      and the context (negation, speculation, ...) '''
 
     for match in response['matches']:
-        print 'id ' + match['id']
+        print 'id ' + match['id'] + " co
