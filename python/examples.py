9a8a67c1d6f15671fa9cda30c1faa09b51ba26ac
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2023-05-15 17:25:19 +0000

    Packstack Based Installation (Storage Components)

--- python/examples.py
+++ python/examples.py
@@ -34,4 +34,4 @@ def exampleEntityExtraction():
 # Entity highlight from a note sample.
 def exampleHighlightEntities():
     url = "https://api.lexigram.io/v1/highlight/entities"
-    text = "The patient was given some hydrocodone for
+    text = "The patient was given some hydrocodone for cont
