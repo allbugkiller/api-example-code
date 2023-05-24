b7abed5360b3d1f6489b4a650f9d6df870fa58f7
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2023-05-24 12:58:40 +0000

    boo

--- python/examples.py
+++ python/examples.py
@@ -39,4 +39,4 @@ def exampleHighlightEntities():
            "and severe hypokalemia. She thinks her potassium might again be low."
 
     r = requests.post(url, data=json.dumps({'text': text}),
-                      headers={'Authoriza
+                      headers={'Authorization': apiKey
