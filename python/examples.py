7cf05c212fe115e87cf2296890fc848fabdf8a43
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2023-05-26 21:30:51 +0000

    Release 0.20.0

--- python/examples.py
+++ python/examples.py
@@ -41,4 +41,4 @@ def exampleHighlightEntities():
     r = requests.post(url, data=json.dumps({'text': text}),
                       headers={'Authorization': apiKey, 'Content-Type': 'application/json'})
 
-    '''Returns the html fo
+    '''Returns the html formated te
