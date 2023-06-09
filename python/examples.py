0e4efdb255c83508dc3b05897022124835ca27de
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2023-06-09 14:51:54 +0000

    add test framework

--- python/examples.py
+++ python/examples.py
@@ -53,4 +53,4 @@ def exampleSearchConcepts():
     r = requests.get(url, headers={'Authorization': apiKey})
     response = json.loads(r.text)
 
-    '''For loop that inspects the response printing to console 
+    '''For loop that inspects the response printing to console the found
