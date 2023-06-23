d3166fd4506e5b0cce5c590cc607ec0d9a00eef6
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2023-06-23 12:39:09 +0000

    Update license and readme

--- python/examples.py
+++ python/examples.py
@@ -58,4 +58,4 @@ def exampleSearchConcepts():
      and the context (negation, speculation, ...) '''
     for hit in response['conceptSearchHits']:
         concept = hit['concept']
-        print 'id ' + concept['id'] + "types", concept
+        print 'id ' + concept['id'] + "types", concept['type
