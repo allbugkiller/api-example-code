cac1946d6a992979f40873da9137a6b4cc6facb9
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2023-07-05 22:45:05 +0000

    v0.0.29

--- python/examples.py
+++ python/examples.py
@@ -69,4 +69,4 @@ def exampleConcept():
     r = requests.get(url, headers={'Authorization': apiKey})
     response = json.loads(r.text)
 
-    # It prints the concept
+    # It prints the concept ID, the type
