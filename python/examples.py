2cf1a44da769d303fb1799a4ce332b8166010e29
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2023-07-21 05:53:16 +0000

    correct formatting

--- python/examples.py
+++ python/examples.py
@@ -76,4 +76,4 @@ def exampleConcept():
 # Ancestors of the concept Id are returned
 def exampleConceptAncestors():
     conceptGraphId = "lxg:49711bf9b46f"  # hearth failure
-    url = "https://api.lexigram.io/v1/lexigraph/concepts/" + conceptGra
+    url = "https://api.lexigram.io/v1/lexigraph/concepts/" + conceptGraphId + "/an
