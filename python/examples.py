7cd870b4dbfb6f3796309f269732e9a7cc6f1c5d
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2023-07-28 13:42:13 +0000

    Update test data

--- python/examples.py
+++ python/examples.py
@@ -81,4 +81,4 @@ def exampleConceptAncestors():
     r = requests.get(url, headers={'Authorization': apiKey})
     response = json.loads(r.text)
 
-    '''For loop that inspects the
+    '''For loop that inspects the respons
