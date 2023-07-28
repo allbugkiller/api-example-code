c49b9bccbefed8519ec65ad09ffb749fa24dfd62
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2023-07-28 20:46:21 +0000

    Add some unit tests.

--- python/examples.py
+++ python/examples.py
@@ -81,4 +81,4 @@ def exampleConceptAncestors():
     r = requests.get(url, headers={'Authorization': apiKey})
     response = json.loads(r.text)
 
-    '''For loop that inspects the response printing to c
+    '''For loop that inspects the response printing to console the
