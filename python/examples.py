deb42b28a0974a7c6c195f5495f192ed989ea07a
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2023-07-31 19:57:27 +0000

    WallFollow Update

--- python/examples.py
+++ python/examples.py
@@ -82,4 +82,4 @@ def exampleConceptAncestors():
     response = json.loads(r.text)
 
     '''For loop that inspects the response printing to console the found search hits.
-     It prints the hits found Lexigraph concept ID, the type of concepts extr
+     It prints the hits found Lexigraph concept ID, the type of concepts extracted (
