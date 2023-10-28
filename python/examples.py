1314b609e6478ce9685f265ae974c4855a29306b
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2023-10-28 17:47:06 +0000

    optimizing

--- python/examples.py
+++ python/examples.py
@@ -90,4 +90,4 @@ def exampleConceptAncestors():
 
 # Descendants of the concept Id are returned
 def exampleConceptDescendants():
-    conceptGraphId = "lxg:49
+    conceptGraphId = "lxg:49711bf9b46f" 
