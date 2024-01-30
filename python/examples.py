9a3f3eb8d390be68c53e3fa0a7f23be5e2e0031d
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2024-01-30 12:55:13 +0000

    sched: Guarantee new group-entities always have weight

--- python/examples.py
+++ python/examples.py
@@ -96,4 +96,4 @@ def exampleConceptDescendants():
     r = requests.get(url, headers={'Authorization': apiKey})
     response = json.loads(r.text)
 
-    '''For loop that inspects the response print
+    '''For loop that inspects the response printing to consol
