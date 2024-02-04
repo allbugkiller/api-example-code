b9fb309affc9f69cdefad7c0901ab175aea09636
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2024-02-04 10:04:10 +0000

    Add missing require.

--- python/examples.py
+++ python/examples.py
@@ -96,4 +96,4 @@ def exampleConceptDescendants():
     r = requests.get(url, headers={'Authorization': apiKey})
     response = json.loads(r.text)
 
-    '''For loop that inspects the response printing to console the fo
+    '''For loop that inspects the response printing to console the found sea
