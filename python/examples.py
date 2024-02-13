9f22cee47ca5e079243bffedf15318f3623b38e3
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2024-02-13 22:27:54 +0000

    Test cases

--- python/examples.py
+++ python/examples.py
@@ -99,3 +99,4 @@ def exampleConceptDescendants():
     '''For loop that inspects the response printing to console the found search hits.
          It prints the hits found Lexigraph concept ID, the type of concepts extracted (problem, drug, etc)'''
     for match in response['results']:
+      
