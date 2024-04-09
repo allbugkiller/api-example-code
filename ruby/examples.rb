8fb5d225b152e9b14db0ee7ec6be7f646f4c550c
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2024-04-09 06:24:16 +0000

    Release v0.9.0

---
+++ ruby/examples.rb
@@ -0,0 +1,12 @@
+
+require 'uri'
+require 'net/http'
+require 'openssl'
+require 'rubygems'
+require 'json'
+
+# This file contains examples of how to interact with Lexigram APIs in Python
+# Each of the examples inspect the responses printing some relevant properties.
+# For comprehensive documentation that includes information of all API responses
+# please visit http://docs.lexigram.io
+api_key = "your_apikey"
