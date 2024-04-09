f3c2715276145e6f420cf53612adaad519ace329
allbugkiller <155300978+allbugkiller@users.noreply.github.com>
2024-04-09 11:12:09 +0000

    Updated todo list

--- ruby/examples.rb
+++ ruby/examples.rb
@@ -10,3 +10,15 @@ require 'json'
 # For comprehensive documentation that includes information of all API responses
 # please visit http://docs.lexigram.io
 api_key = "your_apikey"
+
+def create_http(url)
+  url = URI(url)
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  return http, url
+end
+
+def get(url, api_key, params={})
+  http, url = create_http(url)
+  url.query = URI.encode_www_form(params)
